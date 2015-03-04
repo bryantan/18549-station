68c558e56e4bcc4378945bc0d21a1858dbe699d3
bryantan <bryan.es.tan@gmail.com>
2015-03-04 14:10:07 -0500

    add route to set settings

--- server.py
+++ server.py
@@ -1,15 +1,23 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import json
-#from beacon_scanner import BeaconScanner
+# from beacon_scanner import BeaconScanner
 
 app = Flask(__name__)
 
 
-@app.route('/get_beacons')
-def get_beacons():
-    f = open('dict', 'r')
-    return jsonify(json.loads(f.read()))
-    #Zreturn "Hello World!"
+# @app.route('/get_beacons')
+# def get_beacons():
+#     f = open('dict', 'r')
+#     return jsonify(json.loads(f.read()))
+#     #Zreturn "Hello World!"
+
+
+@app.route('/send-settings', methods=['POST'])
+def send_settings():
+    settings = json.loads(request.form['data'])
+    f = open('settings.conf', 'w')
+    f.write(json.dumps(settings))
+    return
 
 if __name__ == '__main__':
     app.debug = True
