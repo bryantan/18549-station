1e9d83e39641fd39a0a1223d38ff2d15851bff3e
arman23b <arman23b@gmail.com>
2015-04-29 12:53:46 -0400

    Fixed getting ip from ipupdater

--- beacon_scanner.py
+++ beacon_scanner.py
@@ -182,7 +182,7 @@ class BeaconScanner:
         try:
             # get latest IP from hosted site
             r = requests.get('http://placeholder-ipupdater.meteor.com/get-ip')
-            ip_address = json.loads(r.json())['ip']
+            ip_address = r.json()['ip']
             self.save_latest_ip_address(ip_address)
         except Exception as e:
             print "Unable to get IP from webapp: " + str(e)
